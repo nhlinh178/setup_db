f49abbdbaec85be039674407ca7fad765c51f7f1
nhlinh178 <nguyenhuulinh178@gmail.com>
2024-12-06 16:29:09 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ echo "Received PATHPG: $PATHPG"
 echo "Received PATHARCHIVE: $PATHARCHIVE"
 # Cài đặt PostgreSQL 15
 echo "Cài đặt PostgreSQL 15..."
+sudo dnf install -y rsync
 sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 sudo dnf -qy module disable postgresql
 sudo dnf install -y postgresql15-server postgresql15-contrib
