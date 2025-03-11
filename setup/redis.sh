48beaa4ae8768dfa517d92ace122e673fdf8b24d
nhlinh178 <nguyenhuulinh178@gmail.com>
2025-03-11 14:17:32 +0700

    Update redis.sh

--- setup/redis.sh
+++ setup/redis.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Cài đặt Redis"
 sudo yum install redis -y
+echo "sửa file redis.conf"
 sudo sed -i \
     -e "s|^\(#\?\s*\)bind\s*=.*|bind  0.0.0.0|" \
     -e "s|^\(#\?\s*\)port\s*=.*|port 2104|" \
