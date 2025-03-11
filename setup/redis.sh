7fcf88dba5a8d28032ade66e309db9f20ae66d54
nhlinh178 <nguyenhuulinh178@gmail.com>
2025-03-11 14:10:55 +0700

    Create redis.sh

---
+++ setup/redis.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Cài đặt Redis"
+sudo yum install redis -y
+sudo sed -i \
+    -e "s|^\(#\?\s*\)bind\s*=.*|bind = '0.0.0.0'|" \
+    -e "s|^\(#\?\s*\)port\s*=.*|port = '2104'|" \
+    -e "s|^\(#\?\s*\)requirepass\s*=.*|requirepass = 'isofh@2015#Redis'|" \
+    /etc/redis.conf
+sudo systemctl enable redis --now
