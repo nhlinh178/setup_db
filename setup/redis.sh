b101b0702ce25b200b8947a149015db351bf662e
nhlinh178 <nguyenhuulinh178@gmail.com>
2025-03-11 14:30:41 +0700

    Update redis.sh

--- setup/redis.sh
+++ setup/redis.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 echo "Cài đặt Redis"
 sudo yum install redis -y
-echo "sửa file redis.conf"
+
+echo "Sửa file redis.conf 
+sudo cp /etc/redis.conf /etc/redis.conf.bak  
+# Sao lưu file gốc
+# Thay đổi bind và port
 sudo sed -i \
-    -e "s|^\(#\?\s*\)bind\s*=.*|bind  0.0.0.0|" \
-    -e "s|^\(#\?\s*\)port\s*=.*|port 2104|" \
-    -e "s|^\(#\?\s*\)requirepass\s*=.*|requirepass  isofh@2015#Redis|" \
+    -e "s|^bind .*|bind 0.0.0.0|" \
+    -e "s|^port .*|port 2104|" \
+    -e "s|^# requirepass .*|requirepass isofh@2015#Redis|" \
     /etc/redis.conf
+sudo grep -E "^(bind|port|requirepass)" /etc/redis.conf
 sudo systemctl enable redis --now
 echo "Cài đặt hoàn tất Redis"
